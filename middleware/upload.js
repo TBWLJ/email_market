98237d843fe4025adde3d439187e233e13599a1f
TBWLJ <taiwoayomide202@gmail.com>
2025-06-24 18:42:15 +0100

    pepper

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,5 +1,6 @@
 // middleware/upload.ts
-import multer from "multer";
+// import multer from "multer";
+const multer = require("multer");
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
